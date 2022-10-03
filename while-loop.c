f1e74d61cef0ec754eb6a616c0fef59015692207
Lema05 <lemakipeno@gmail.com>
2022-10-03 23:52:35 +0300

    C function that performs while loops.

---
+++ while-loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+
+char name[25];
+
+printf("\nWhats's your name?: ");
+fgets(name, 25, stdin);
+name[strlen(name)-1] = '\0';
+
+printf("Hello %s", name);
+
+while(strlen(name) == 0){
+    printf("\nYoU dd not enter your name")
+    printf("\nWhat's your name? ");
+    name[strlen(name)-1] = '\0';
+
+}
+
+
+
+
+return 0;
+
+}
