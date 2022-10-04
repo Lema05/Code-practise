73f08112af527157c6f50432752a6f92826f0e5d
Lema05 <lemakipeno@gmail.com>
2022-10-04 12:47:16 +0300

    pointers in c

---
+++ pointers-c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+// Pointer = a variable like reference that holds a memory adress of another variable
+
+    int age = 21;
+    int *pAge = &age;
+
+    printf("adress of age: %p\n", &age);
+    printf("value of Page: %p\n", pAge);
+
+
+    return 0;
+
+
+
+
+
+
+}
